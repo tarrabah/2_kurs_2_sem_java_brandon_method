8e754f8855898b6ca1d6b4d69c45eade81f35210
Vlad_Zakrevsky <zakrevsky.vladislav@gmail.com>
2022-06-02 21:11:50 +0300

    changed the last plot to scatter and now table is empty on start

--- src/main/java/src/TabWidget.java
+++ src/main/java/src/TabWidget.java
@@ -5,6 +5,7 @@ import javax.swing.*;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.xy.XYSeriesCollection;
 
 class TabWidget extends JTabbedPane
@@ -32,24 +33,23 @@ class TabWidget extends JTabbedPane
         //first chart
         x1Dataset = new XYSeriesCollection();
         chartX1 = ChartFactory.createXYLineChart("y(x1))",
-                "x1", "y(x1)", x1Dataset);
+                "x1", "y(x1)", x1Dataset, PlotOrientation.VERTICAL, false, false, false);
         firstParameterPLot = new ChartPanel(chartX1);
 
         x2Dataset = new XYSeriesCollection();
         chartX2 = ChartFactory.createXYLineChart("y(x2))",
-                "x2", "y(x2)", x2Dataset);
+                "x2", "y(x2)", x2Dataset, PlotOrientation.VERTICAL, false, false, false);
         secondParameterPLot = new ChartPanel(chartX2);
 
         x3Dataset = new XYSeriesCollection();
         chartX3 = ChartFactory.createXYLineChart("y(x3))",
-                "x3", "y(x3)", x3Dataset);
+                "x3", "y(x3)", x3Dataset, PlotOrientation.VERTICAL, false, false, false);
         thirdParameterPLot = new ChartPanel(chartX3);
 
         resultDataset = new XYSeriesCollection();
-        chartResult = ChartFactory.createXYLineChart("y(n))",
-                "n", "y(n)", resultDataset);
+        chartResult = ChartFactory.createScatterPlot("y(n)",
+                "n", "y(n)", resultDataset, PlotOrientation.VERTICAL, false, false, false);
         resultPLot = new ChartPanel(chartResult);
-
         //secondParameterPLot = new ChartPanel();
         //thirdParameterPLot = new ChartPanel();
         //resultPLot = new ChartPanel();
